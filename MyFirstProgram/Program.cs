57dd79553a60b27df41e5049fe9a7270e993ccdb
marti <marti@DESKTOP-4V4K5JI>
2023-09-12 12:13:42 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿//namn: Martin Líndenhöök
+//klass: NET23
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //number variable
+            int num = 4;
+
+            //checks if the variable is higher than 10.
+            if (num > 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+
+            //checks if the variable is lower than 10.
+            else if (num < 10)
+            {
+                Console.WriteLine("Oj. Lågt tal!");
+            }
+
+            Console.WriteLine("Vad heter du?\n");
+            
+            //reads user input from cli what their name is.
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hej {name}!");
+
+            for(int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+        
+        }
+    }
+}
